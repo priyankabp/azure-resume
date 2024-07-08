f6e1ad7448ed7ead66249b63b43f850b9d4cc224
Priyanka Phapale <pbphapale@gmail.com>
2024-07-08 14:19:01 -0700

    Created azure function api

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionApi = '';
+const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
@@ -12,7 +12,7 @@ const getVisitCount = () => {
         console.log("Website called function API.");
         count = response.count;
         document.getElementById("counter").innerText = count;
-    }).catch(function(error){
+    }).catch(function (error) {
         console.log(error);
     });
     return count;
