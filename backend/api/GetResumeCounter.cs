ca62fce708ec9a0fddfc74600c6bfc8f20830006
Priyanka Phapale <pbphapale@gmail.com>
2024-07-12 15:28:32 -0700

    Updates to function and backend workflow

--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -16,12 +16,13 @@ namespace Company.Function
     public static class GetResumeCounter
     {
         [FunctionName("GetResumeCounter")]
-        public static  HttpResponseMessage Run(
+        public static HttpResponseMessage Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            [CosmosDB(databaseName:"AzureResume", containerName: "Counter" , Connection = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] Counter counter,
-            [CosmosDB(databaseName:"AzureResume", containerName: "Counter" , Connection = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] out Counter updatedCounter,
+            [CosmosDB(databaseName: "AzureResume", containerName: "Counter", Connection = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] Counter counter,
+            [CosmosDB(databaseName: "AzureResume", containerName: "Counter", Connection = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] out Counter updatedCounter,
             ILogger log)
         {
+            // Here is where the counter gets updated.
             log.LogInformation("C# HTTP trigger function processed a request.");
             updatedCounter = counter;
             updatedCounter.Count += 1;
